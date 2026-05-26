27a7aaca574fada4c19d3efc6b5ebba2c384fe43
valuedesign-yoshihara <yoshihara.valuedesign@gmail.com>
2026-05-26 13:31:18 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -28,11 +28,11 @@ export default defineConfig({
     react(),
     // 👇 PWAの設定を追加
     VitePWA({
-      strategies: 'generateSW', // 💡 明示的に生成ストラテジーを指定
-      outDir: 'dist',           // 💡 出力先が dist であることをプラグイン側にも叩き込む      registerType: 'autoUpdate',
-      injectRegister: 'inline', // 💡 サブディレクトリ（/DemoPos/）対策としてこれを追加！
+      strategies: 'generateSW',
+      outDir: 'dist',
+      injectRegister: 'inline',
       filename: 'manifest.json',
-      includeAssets: ['favicon.png', 'pwa-192x192.png', 'pwa-512x512.png'], // 💡 実際に存在するファイル名に合わせる
+      includeAssets: ['favicon.png', 'favicon_192x192.png', 'favicon_512x512.png'], // 💡 実際に存在するファイル名に合わせる
       manifest: {
         name: '売店 POS System',
         short_name: '売店POS',
